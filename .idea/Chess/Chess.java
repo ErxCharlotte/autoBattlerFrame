1b9460b3357fb7e29233fbc71c78ff6d99f2109a
ErxCharlotte <878658607@qq.com>
2023-11-06 16:52:19 +1100

    create the chess interface.

---
+++ .idea/Chess/Chess.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import Chess.*;
+
+public interface Chess {
+    private double HP;
+    private double MP;
+    private int ad;
+    private int ap;
+    private int ar;
+    private int mr;
+    private int range;
+    private int speed;
+    private ArrayList damageList;
+    private Chess target;
+    private int cost;
+    private int positionX;
+    private int positionY;
+    private int level;
+    private Status status;
+
+
+
+
+
+
+}
